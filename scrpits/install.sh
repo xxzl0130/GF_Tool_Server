3bd64d3e72326fa75122f7e99a628aad6431fc82
Kiriko_X <42537180+KirikoXX@users.noreply.github.com>
2019-11-14 20:31:57 +0800

    fix install

--- scrpits/install.sh
+++ scrpits/install.sh
@@ -4,6 +4,6 @@ unzip -o -d ./GF_Tool_Server GF_Tool_Server_linux.zip
 mkdir /etc/GF_Tool_Server
 chmod +x ./GF_Tool_Server/GF_Tool_Server
 cp -f ./GF_Tool_Server /etc/GF_Tool_Server
-cp ./GF_Tool_Server/GF_Server /etc/init.d
+cp ./GF_Tool_Server/scrpits/GF_Server /etc/init.d
 chmod 755 /etc/init.d/GF_Server
 update-rc.d GF_Server defaults 95
