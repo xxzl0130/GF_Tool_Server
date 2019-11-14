b0cd3fcc2ae231288441d5b3c3ca8382c747d7f1
Kiriko_X <42537180+KirikoXX@users.noreply.github.com>
2019-11-14 21:08:08 +0800

    fix bug

--- scrpits/install.sh
+++ scrpits/install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-wget https://github.com/xxzl0130/GF_Tool_Server/releases/download/v1.0/GF_Tool_Server_linux.zip
+wget https://github.com/xxzl0130/GF_Tool_Server/releases/download/v1.1/GF_Tool_Server.zip
 unzip -o -d ./GF_Tool_Server GF_Tool_Server_linux.zip
-mkdir /etc/GF_Tool_Server
 chmod +x ./GF_Tool_Server/GF_Tool_Server
-cp -f ./GF_Tool_Server /etc/GF_Tool_Server
+cp -rf ./GF_Tool_Server /etc/
 cp ./GF_Tool_Server/scrpits/GF_Server /etc/init.d
 chmod 755 /etc/init.d/GF_Server
 update-rc.d GF_Server defaults 95
